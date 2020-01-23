0eac15710ef05f7bb60bb0060a443a4a2e85bf67
Nilton Lopes <nilton.lopes@madeiramadeira.com.br>
2020-01-23 14:20:25 -0300

    Calcula montante juros compostos

---
+++ week-03/exercise-01-tests/montante-juros-compostos.js
@@ -0,0 +1,15 @@
+/**
+ * Recebe capital, juiros, tempo e calcula o montante que resultante dessa operação
+ * @param {*} capital inteiro ou float com a quantidade do capital
+ * @param {*} juros em porcentagem. Ex: para 50% fornecer 50
+ * @param {*} tempo quantidade de tempo que o calculo deve ser realizado
+ */
+const montanteJurosCompostos = (capital, juros, tempo) => {
+  const jurosDecimal = juros / 100
+  const montante = capital * (1 + jurosDecimal) ** tempo
+  return montante
+}
+
+module.exports = {
+  montanteJurosCompostos
+}
