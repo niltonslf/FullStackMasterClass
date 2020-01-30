a622a2aa40af550245eda77a2b405026fd96f8dc
Nilton Lopes <niltonrck@gmail.com>
2020-01-29 21:31:43 -0300

    Move todos os métodos de calculo de juros para o juros.js, adiciona mais testes

--- week-03/exercise-01-tests/juros.js
+++ week-03/exercise-01-tests/juros.js
@@ -1,8 +1,55 @@
-function calcJuros(capital, juros, tempo) {
+/**
+ * Realiza calculo de juros simples com base no juros em decimal e tempo
+ * @param {*} capital
+ * @param {*} juros
+ * @param {*} tempo
+ */
+function jurosSimples(capital, juros, tempo) {
   const jurosDecimal = juros / 100
   return parseFloat(capital) * parseFloat(jurosDecimal) * tempo
 }
 
+/**
+ *
+ * @param {Object} dependecies
+ */
+const montanteSimples = ({ jurosSimples }) => {
+  return (capital, juros, tempo) => {
+    const jurosSimplesRes = jurosSimples(capital, juros, tempo)
+    return capital + jurosSimplesRes
+  }
+}
+
+/**
+ * Recebe capital, juiros, tempo e calcula o montante que resultante dessa operação
+ * @param {*} capital inteiro ou float com a quantidade do capital
+ * @param {*} juros em porcentagem. Ex: para 50% fornecer 50
+ * @param {*} tempo quantidade de tempo que o calculo deve ser realizado
+ */
+const montanteJurosCompostos = (capital, juros, tempo) => {
+  const jurosDecimal = juros / 100
+  const montante = capital * (1 + jurosDecimal) ** tempo
+
+  return montante
+}
+
+/**
+ * Retorna o valor de juros em cima de um montante
+ * @param {*} capital Valor inicial sem o juros
+ * @param {*} montante Valor final já com o juros aplicado
+ */
+const jurosCompostos = ({ montanteJurosCompostos }) => {
+  return (capital, juros, tempo) =>
+    montanteJurosCompostos(capital, juros, tempo) - capital
+}
+
 module.exports = {
-  calcJuros
+  jurosSimples,
+  montanteSimples: montanteSimples({ jurosSimples }),
+  montanteJurosCompostos,
+  jurosCompostos: jurosCompostos({ montanteJurosCompostos }),
+  pure: {
+    montanteSimples,
+    jurosCompostos
+  }
 }
--- week-03/exercise-01-tests/juros.test.js
+++ week-03/exercise-01-tests/juros.test.js
@@ -1,8 +1,60 @@
-const { calcJuros } = require('./juros')
+const {
+  jurosSimples,
+  extrairJuros,
+  montanteJurosCompostos,
+  pure
+} = require('./juros')
+const { montanteSimples, jurosCompostos } = pure
 
 describe('Calcular juros', () => {
-  test('calcJuros', () => {
-    const juros = calcJuros(1000, 50, 1)
+  test('JurosSimples', () => {
+    const juros = jurosSimples(1000, 50, 1)
     expect(juros).toBe(500)
+  })
+  test('JurosSimples tempo = 0', () => {
+    const juros = jurosSimples(1000, 50, 0)
+    expect(juros).toBe(0)
+  })
+})
+
+describe('Montate juros', () => {
+  test('calcMontateJurosSimples', () => {
+    const jurosSimples = jest.fn()
+    jurosSimples.mockReturnValue(500)
+
+    const resJuros = montanteSimples({ jurosSimples })(100, 5, 1)
+    expect(resJuros).toBe(600)
+  })
+})
+
+describe('Montate juros compostos', () => {
+  test('montanteJurosCompostos', () => {
+    const capital = 1000
+    const juros = 50
+    const tempo = 1
+
+    const res = montanteJurosCompostos(capital, juros, tempo)
+
+    expect(res).toStrictEqual(1500)
+  })
+
+  test('JurosCompostos ', () => {
+    const capital = 1000
+    const juros = 10
+    const tempo = 1
+
+    const montanteJurosCompostos = jest.fn()
+    montanteJurosCompostos.mockImplementation(() => 1100)
+
+    const jurosRes = jurosCompostos({
+      montanteJurosCompostos
+    })(capital, juros, tempo)
+
+    expect(montanteJurosCompostos.mock.calls[0]).toEqual([
+      capital,
+      juros,
+      tempo
+    ])
+    expect(jurosRes).toBe(100)
   })
 })
