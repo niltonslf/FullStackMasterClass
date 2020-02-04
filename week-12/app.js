5afd1802be8500731441b85922fe2f06868ee12e
Nilton Lopes <niltonrck@gmail.com>
2020-02-04 05:44:54 -0300

    inicia semana 12

---
+++ week-12/app.js
@@ -0,0 +1,83 @@
+const express = require("express")
+const mongoose = require("mongoose")
+const bodyParser = require("body-parser")
+const jwt = require("jsonwebtoken")
+const cors = require("cors")
+
+const User = require("./models/user")
+const jwtSecret = "!@#$%"
+
+const app = express()
+mongoose.Promise = global.Promise
+
+const port = process.env.PORT || 3333
+const mongo =
+  process.env.MONGO ||
+  "mongodb+srv://niltonslf:mongo123@cluster0-hakdt.mongodb.net/series?retryWrites=true&w=majority"
+
+const series = require("./routes/series")
+const users = require("./routes/users")
+
+app.use(cors())
+app.use(bodyParser.json())
+app.use("/series", series)
+app.use("/users", users)
+
+app.post("/auth", async (req, res) => {
+  const { username, password } = req.body
+
+  const userDb = await User.findOne({ username })
+
+  if (userDb) {
+    if (userDb.password == password) {
+      const payload = {
+        id: userDb._id,
+        username: userDb.username,
+        roles: userDb.roles
+      }
+      jwt.sign(payload, jwtSecret, (err, token) => {
+        res.send({
+          success: true,
+          token
+        })
+      })
+    } else {
+      res.send({
+        success: false,
+        message: "Wrong credentials"
+      })
+    }
+  } else {
+    res.send({
+      success: false,
+      message: "Wrong credentials"
+    })
+  }
+})
+
+const createInitialUsers = async () => {
+  const total = await User.count({})
+  if (total == 0) {
+    const user = new User({
+      username: "niltonslf",
+      password: 123456,
+      roles: ["restrito", "admin"]
+    })
+    await user.save()
+
+    const user2 = await new User({
+      username: "guest",
+      password: 123456,
+      roles: ["restrito"]
+    })
+
+    await user2.save()
+  }
+}
+
+mongoose.connect(mongo, { useNewUrlParser: true }).then(() => {
+  createInitialUsers()
+  app.listen(port, () => {
+    console.log(`Server was started on http://localhost:${port}`)
+  })
+})
---
+++ week-12/routes/series.js
@@ -0,0 +1,87 @@
+const { Router } = require("express")
+const Serie = require("../models/serie")
+
+const jwt = require("jsonwebtoken")
+const jwtSecret = "!@#$%"
+
+const router = Router()
+
+router.use(async (req, res, next) => {
+  const token =
+    req.headers["x-access-token"] || req.body.token || req.query.token
+
+  if (token) {
+    try {
+      const payload = jwt.verify(token, jwtSecret)
+
+      if (payload.roles.includes("restrito")) {
+        next()
+      } else {
+        res.send({
+          success: false,
+          message: "You dont have permission to access this page."
+        })
+      }
+    } catch (e) {
+      res.send({
+        success: false,
+        message: "unauthorized"
+      })
+    }
+  }
+  res.res({
+    success: false,
+    message: "unauthorized"
+  })
+})
+
+router.get("/", async (req, res) => {
+  const series = await Serie.find({})
+
+  res.send(series)
+})
+
+router.post("/", async (req, res) => {
+  const serie = new Serie(req.body)
+  try {
+    await serie.save()
+    res.json(serie)
+  } catch (error) {
+    res.send({
+      success: false,
+      erros: Object.keys(error.errors)
+    })
+  }
+})
+
+router.delete("/:id", async (req, res) => {
+  await Serie.remove({ _id: req.params.id })
+  res.send({
+    success: true
+  })
+})
+
+router.get("/:id", async (req, res) => {
+  const serie = await Serie.findOne({ _id: req.params.id })
+  res.json(serie)
+})
+
+router.put("/:id", async (req, res) => {
+  const serie = await Serie.findOne({ _id: req.params.id })
+  const { name, status, comments } = req.body
+
+  serie.name = name
+  serie.status = status
+
+  try {
+    await serie.save()
+    res.json(serie)
+  } catch (e) {
+    res.send({
+      success: false,
+      erros: e
+    })
+  }
+})
+
+module.exports = router
