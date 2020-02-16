703eda7616bf5cb833f8c14d46ab17d9e91b4baf
Nilton Lopes <nilton.lopes@madeiramadeira.com.br>
2020-02-16 17:16:29 -0300

    Adiciona mais testes E2E

--- week-12/tests/api.test.js
+++ week-12/tests/api.test.js
@@ -4,11 +4,33 @@ const expect = require("chai").expect
 const mongoose = require("mongoose")
 const mongo =
   "mongodb://localhost:27017/test-series?retryWrites=true&w=majority"
-const User = require("../models/user")
+mongoose.Promise = global.Promise
 
 const app = require("../app")
 
+const User = require("../models/user")
+const Serie = require("../models/serie")
+
 describe("Testing Rest API", () => {
+  before("Connection to mongodb", async () => {
+    await mongoose.connect(mongo, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    })
+    // Remover todos os usuários
+    await User.deleteMany({})
+
+    // Criar um usuário de teste
+    const user = new User({
+      username: "niltonslf",
+      password: 123456,
+      roles: ["admin", "restrito"]
+    })
+    await user.save()
+    await Serie.deleteMany({})
+    return true
+  })
+
   it("should return an error to series ", done => {
     request(app)
       .get("/series")
@@ -20,26 +42,8 @@ describe("Testing Rest API", () => {
         done()
       })
   })
-})
 
-describe("Testing /auth", () => {
-  mongoose
-    .connect(mongo, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => {
-      // Remover todos os usuários
-      User.collection.deleteMany({})
-
-      // Criar um usuário de teste
-      const user = new User({
-        username: "niltonslf",
-        password: 123456,
-        roles: ["admin"]
-      })
-
-      user.save()
-    })
-
-  it("shoud retun an auth token", done => {
+  it("should auth an user", done => {
     request(app)
       .post("/auth")
       .send({
@@ -51,12 +55,13 @@ describe("Testing /auth", () => {
       .expect(200)
       .end((err, res) => {
         expect(err).be.null
+        userToken = res.body.token
         expect(res.body.token).be.string
         done()
       })
   })
 
-  it("Should return an error to auth", done => {
+  it("Should not auth an user", done => {
     request(app)
       .post("/auth")
       .send({})
@@ -69,5 +74,50 @@ describe("Testing /auth", () => {
         expect(res.body.message).be.string
         done()
       })
+  })
+
+  describe("auth as admin", () => {
+    let token = ""
+    before("get token", done => {
+      request(app)
+        .post("/auth")
+        .send({ username: "niltonslf", password: 123456 })
+        .set("Accept", "application/json")
+        .expect("Content-Type", /json/)
+        .expect(200)
+        .end((err, res) => {
+          expect(err).be.null
+          userToken = res.body.token
+          token = res.body.token
+          done()
+        })
+    })
+
+    it("Should return no series", done => {
+      request(app)
+        .get("/series")
+        .set("x-access-token", token)
+        .expect("Content-Type", /json/)
+        .expect(200)
+        .end((err, res) => {
+          expect(err).be.null
+          expect(res.body).be.empty
+          done()
+        })
+    })
+
+    it("sould return a new series", done => {
+      request(app)
+        .post("/series")
+        .set("x-access-token", token)
+        .send({ name: "Minha serie", status: "to-watch" })
+        .expect("Content-Type", /json/)
+        .expect(200)
+        .end((err, res) => {
+          expect(err).be.null
+          expect(res.body).not.empty
+          done()
+        })
+    })
   })
 })
