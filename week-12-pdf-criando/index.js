8a5460b3c09463eb525f80c2a43c6d98bcf6aaff
Nilton Lopes <nilton.lopes@madeiramadeira.com.br>
2020-02-16 17:42:10 -0300

    Adiciona tabela de usuários e estilos no pdf

--- week-12-pdf-criando/index.js
+++ week-12-pdf-criando/index.js
@@ -11,8 +11,54 @@ const fonts = {
 }
 
 const printer = new PdfPrinter(fonts)
+
+const lines = []
+lines.push([
+  {
+    text: "Nome",
+    style: "header"
+  },
+  {
+    text: "E-mail",
+    style: "header"
+  },
+  {
+    text: "Situação",
+    style: "header"
+  }
+])
+
+for (let i = 0; i < 300; i++) {
+  let ativo = "Ativo"
+  if (i % 2 == 0) {
+    ativo = {
+      text: "Inativo",
+      style: "inativo"
+    }
+  }
+
+  lines.push(["Nilton", "niltonrck@gmail.com", ativo])
+}
+
 const docDefinition = {
-  content: "Fullstack Master"
+  content: [
+    { text: "Fullstack Master" },
+    {
+      table: {
+        widths: ["*", "*", 100],
+        body: lines
+      }
+    }
+  ],
+  styles: {
+    header: {
+      fontSize: 18,
+      bold: true
+    },
+    inativo: {
+      color: "red"
+    }
+  }
 }
 
 const pdf = printer.createPdfKitDocument(docDefinition)
