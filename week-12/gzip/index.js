6d4a83d543a34991dae42c417fa8425a0a21ceb6
Nilton Lopes <nilton.lopes@madeiramadeira.com.br>
2020-02-19 08:01:44 -0300

    controlando cache da página

--- week-12/gzip/index.js
+++ week-12/gzip/index.js
@@ -3,21 +3,20 @@ const compression = require('compression')
 
 
 const app = express()
-app.use(compression({level: 9}))
+app.use(compression())
 
 const largeObject = []
+for (let i = 0; i < 10000; i++) {
+  largeObject.push({
+    name:'Nilton',
+    age: 23,
+    key: i
+  })
+  
+}
 
 app.get('/', (req,res) => {
-  for (let i = 0; i < 10000; i++) {
-    largeObject.push({
-      name:'Nilton',
-      age: 23,
-      key: i
-    })
-    
-  }
-  
-  
+  res.header('Cache-Control', 'public, max-age=3600')
   res.json(largeObject)
 })
 
