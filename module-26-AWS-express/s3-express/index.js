76e0a11bb0336d3fc6132649b5dc6926d371306e
Nilton Lopes <nilton.lopes@madeiramadeira.com.br>
2020-04-24 14:44:19 -0300

    Faz envio dos arquivos para o s3 e adiciona o sequelize

--- module-26-AWS-express/s3-express/index.js
+++ module-26-AWS-express/s3-express/index.js
@@ -8,8 +8,9 @@ const path = require("path")
 
 const s3 = require("s3")
 const multer = require("multer")
-const { get } = require("http")
 const uploader = multer({ dest: "uploads" })
+
+const Sequelize = require("sequelize")
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
@@ -17,35 +18,100 @@ app.use(express.json())
 app.set("view engine", "ejs")
 app.set("views", path.join(__dirname, "views"))
 
-const options = {
-  s3Options: {
-    accessKeyId: process.env.ACCESS_KEY_ID,
-    secretAccessKey: process.env.SECRET_ACCESS_KEY,
-    region: process.env.AWS_DEFAULT_REGION,
+const s3Config = {
+  accessKeyId: process.env.ACCESS_KEY_ID,
+  secretAccessKey: process.env.SECRET_ACCESS_KEY,
+  region: process.env.AWS_DEFAULT_REGION,
+  bucket: "fullstack-master-nslf",
+}
+
+const sequelize = new Sequelize("arquivos", "admin", "admin", {
+  host: "localhost",
+  dialect: "mysql",
+})
+
+const Arquivo = sequelize.define("arquivo", {
+  name: {
+    type: Sequelize.STRING,
+    field: "name",
   },
+})
+
+const options = {
+  s3Options: s3Config,
 }
 
 const s3Client = s3.createClient(options)
+const aws = require("aws-sdk")
+aws.config = new aws.Config(s3Config)
+const s3SDK = new aws.S3()
 
-app.get("/", (req, res) => res.render("index"))
-
-app.post("/upload", uploader.single("file"), (req, res) => {
+const uploadToS3 = (file, key, mimetype, s3Config) => {
   const fileParams = {
-    localFile: req.file.path,
+    localFile: file,
     s3Params: {
-      Bucket: "fullstack-master-nslf",
-      Key: req.file.originalname,
+      Bucket: s3Config.bucket,
+      Key: key,
+      ContentType: mimetype,
+      // ACL: "public-read",
     },
   }
-  const uploader = s3Client.uploadFile(fileParams)
 
-  uploader.on("end", () => {
-    fs.unlinkSync(req.file.path)
-    res.send("upload finalizado")
+  return new Promise((resolve, reject) => {
+    const uploader = s3Client.uploadFile(fileParams)
+    uploader.on("end", () =>
+      resolve({ message: "Arquivo enviado com sucesso", body: file })
+    )
+    uploader.on("error", (error) =>
+      reject({
+        message: "erro ao enviar arquivo",
+        error,
+      })
+    )
   })
-  uploader.on("error", () => {
-    console.log("erro ao enviar arquivo")
+}
+
+const removeFile = (file) => {
+  return new Promise((resolve, reject) => {
+    fs.unlink(file, (err) => {
+      if (err) reject()
+      else resolve()
+    })
   })
+}
+
+app.get("/", async (req, res) => {
+  const arquivos = await Arquivo.findAll()
+
+  res.render("index", { arquivos })
 })
 
-app.listen(PORT, () => console.log("listening..."))
+app.get("/ver/:id", async (req, res) => {
+  const arquivo = await Arquivo.findByPk(req.params.id)
+
+  const s3File = {
+    Bucket: s3Config.bucket,
+    Key: arquivo.name,
+    Expires: 10,
+  }
+  signedUrl = s3SDK.getSignedUrl("getObject", s3File)
+
+  res.redirect(signedUrl)
+})
+
+app.post("/upload", uploader.single("file"), async (req, res) => {
+  try {
+    const { path, originalname, mimetype } = req.file
+    await uploadToS3(path, originalname, mimetype, s3Config)
+    await removeFile(path)
+    await Arquivo.create({ name: originalname })
+
+    res.redirect("/")
+  } catch (error) {
+    console.log(error)
+  }
+})
+
+sequelize.sync().then(() => {
+  app.listen(PORT, () => console.log("listening..."))
+})
